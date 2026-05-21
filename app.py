7f3bb329dbb6997d5367d8ad297a0017ef6dd184
Klebson Davi <35002746+krepss@users.noreply.github.com>
2026-05-21 14:52:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import time
 # ==========================================
 st.set_page_config(page_title="Sistema Operacional 360", page_icon="🎯", layout="wide")
 
-GESTOR_EMAIL = "admin@brisanet.com.br"
+GESTOR_EMAIL = "gestor"
 GESTOR_SENHA = "admin"
 SENHA_PADRAO_AGENTE = "1234" 
 
